e5f1692e0a6e16b30b7cabb3ab260724c24341fd
Anderson Luís Furlan <alsfurlan@gmail.com>
2024-08-26 21:46:42 -0300

    Corrigindo espaço em branco no hello world

--- hello-world.js
+++ hello-world.js
@@ -1,7 +1,7 @@
 function helloWorld() {
     const input = document.querySelector('#name');
     const p = document.querySelector('#helloWorldMessage');
-    if (input.value) {
+    if (input.value.trim()) {
         p.textContent = 'Bem-vindo à programação móvel ' + input.value + "!";
     } else {
         p.textContent = '';
